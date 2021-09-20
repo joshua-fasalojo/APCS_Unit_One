a1082337f6a2809d6cdcaeca049bdac56c32c4d7
joshua-fasalojo <fasalif@ssfs.org>
2021-09-20 09:31:55 -0400

    Getting user input

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -4,5 +4,17 @@ public class DateFormat {
 
     public static void main(String[] args) {
 
-    }
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is the current month? ");
+    String Month = scan.nextLine();
+    System.out.println("What is the day of the week? ");
+    String day = scan.nextLine();
+    System.out.println("What is the current year? ");
+    int year = scan.nextInt();
+    System.out.println("What is the current day of the month? ");
+    int dayOfMonth = scan.nextInt();
+    System.out.println(" American Format  ");
+    System.out.println(day+"," + Month + dayOfMonth + "," + year );
+    System.out.println(" European Format ");
+    System.out.println(day+"," + dayOfMonth + "," + Month + year );
 }
--- src/MetaJava.java
+++ src/MetaJava.java
@@ -1,6 +1,8 @@
-public class MetaJava {
-
+public cl1 ass MetaJava {
     public static void main(String[] args) {
-
+        pclass Hello {
+            public static void main(String[] args) {
+                System.out.println("Hello, world!");
+            }
+        }
     }
-}
