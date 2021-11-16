95039b22a61c13275c7301ba4162fc9a50c88334
joshua-fasalojo <fasalif@ssfs.org>
2021-11-15 22:28:46 -0500

    TempConversion

--- src/TemperatureConversion.java
+++ src/TemperatureConversion.java
@@ -3,11 +3,16 @@ public class TemperatureConversion {
     public static void main(String[] args) {
 
         // Create a variable of the appropriate type and initialize to to 78 degrees
+        int farenheit = 78;
 
         // Create a second variable and write an expression to convert the value above to Celsius
+        double conversion = farenheit-32 * 9/5.49;
+        System.out.println(conversion);
 
         // Display the results to the user:
         // ex. 212 degrees F is 100 degrees C
+        System.out.println(farenheit + " degrees Fahrenheit is " + conversion +  " degrees Celsius ");
+
 
 
     }
--- src/TotalWithTax.java
+++ src/TotalWithTax.java
@@ -7,9 +7,9 @@ public class TotalWithTax {
         double price = 52.75;
         double tax = TAXRATE*price;
         double total = price + tax;
-        System.out.println(tax);
+
          tax = 4.351875000000001;
-        System.out.println(price + tax);
+
         total = 57.101875;
         System.out.println("Price: $" + price);
         System.out.println("Sales Tax: $" + tax );
