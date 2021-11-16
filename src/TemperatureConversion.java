13e7024184788156bc320fd260af8d8127f20bbf
joshua-fasalojo <fasalif@ssfs.org>
2021-11-15 22:36:19 -0500

    TempConversion

--- src/TemperatureConversion.java
+++ src/TemperatureConversion.java
@@ -6,12 +6,13 @@ public class TemperatureConversion {
         int farenheit = 78;
 
         // Create a second variable and write an expression to convert the value above to Celsius
-        double conversion = farenheit-32 * 9/5.49;
+        double conversion = 78-32;
         System.out.println(conversion);
+        System.out.println(conversion/1.8);
 
         // Display the results to the user:
         // ex. 212 degrees F is 100 degrees C
-        System.out.println(farenheit + " degrees Fahrenheit is " + conversion +  " degrees Celsius ");
+        System.out.println(farenheit + " degrees Fahrenheit is " + conversion/1.8 +  " degrees Celsius ");
 
 
 
