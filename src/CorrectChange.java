b81b6d3468dfdfa7ea8c7c2719886002530781b5
joshua-fasalojo <fasalif@ssfs.org>
2021-11-16 14:06:38 -0500

    Correctchange

--- src/CorrectChange.java
+++ src/CorrectChange.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class CorrectChange {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many cents in total?");
+        int cents = input.nextInt();
+        int dollar = (cents/100) ;
+        cents = (cents - (100*dollar));
+        int quarter = (cents/25);
+        cents = (cents - (25*quarter));
+        int dime = (cents/10);
+        cents = (cents - (10*dime));
+        int nickel = (cents/5);
+        cents = (cents - (5*nickel));
+        int pennies = (cents/1);
+        cents = (cents - (1*pennies));
+        System.out.println("You have " + dollar + " dollars," + quarter + " quarters," + dime + " dimes," + nickel + " nickels," + " and " + pennies + " pennies."  );
+
+
+
 
     }
 }
