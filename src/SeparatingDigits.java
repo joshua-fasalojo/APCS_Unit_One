35a44bad36da4f3e09f781e9c86283b0417cf802
joshua-fasalojo <fasalif@ssfs.org>
2021-11-24 09:30:16 -0500

    SeperatingDigits

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -4,11 +4,10 @@ public class SeparatingDigits {
 
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.println("Please enter a whole number: ");
-        int individualDigits = input.nextInt();
-        int threeSpaceDigits = input.nextInt();
-        System.out.println("Please space out number: ");
-        System.out.println("Please enter three spaces between each number: ");
+        String newEmpty = "";
+        System.out.println("a five digit number:  ");
+
+
 
 
     }
