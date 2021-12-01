18d31ae1bd59575700adeb7a4e24f45d5647270b
joshua-fasalojo <fasalif@ssfs.org>
2021-12-01 16:40:21 -0500

    FindTheAverage

--- src/FindTheAverage.java
+++ src/FindTheAverage.java
@@ -5,12 +5,19 @@ public class FindTheAverage {
     public static void main(String[] args) {
 
         // Create a Scanner object to get user input
-
+            Scanner input = new Scanner (System.in);
         // Create a total variable to keep track of the total value
-
+            String totalValue = "";
         // Get four pieces of user input
+        System.out.println("Enter a number with a decimal");
+        System.out.println("Enter a number with a decimal");
+        System.out.println("Enter a number with a decimal");
+        System.out.println("Enter a number with a decimal");
 
         // Calculate the average
+
+
+
 
         // Display the result
 
